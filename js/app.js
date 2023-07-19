e105c45cae598f3c49ef957ce070f73b509a8177
SebastianHdzMiranda <sebashm68@gmail.com>
2023-07-19 17:16:45 -0600

    creando e instanciando classe de presupuesto

--- js/app.js
+++ js/app.js
@@ -11,17 +11,31 @@ function eventListeners() {
 
 
 // Classes
+class Presupuesto {
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+class UI{
 
+}
 
+let presupuesto; 
+// instanciar UI
+const ui = new UI();
 
 // Funciones
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cual es tu Presupuesto?');
 
-    console.log(presupuestoUsuario); //me tranforma a numero
-    console.log(isNaN(presupuestoUsuario));
+    console.log(Number(presupuestoUsuario)); //me tranforma a numero
+    console.log(isNaN(presupuestoUsuario)); //me dice si es un numero o no en boolean
 
     if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload(); //me refresca la pagina
     }
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto); 
 }
