4bac4efdca3a20c88c2544ec90d27b9e91c79828
SebastianHdzMiranda <sebashm68@gmail.com>
2023-07-19 18:15:38 -0600

    trabajando en el metodo insertarPresupuesto de la classe UI

--- js/app.js
+++ js/app.js
@@ -19,7 +19,13 @@ class Presupuesto {
     }
 }
 class UI{
+    insertarPresupuesto( cantidad ){
+        console.log(cantidad);
+        const {presupuesto, restante} = cantidad;
 
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
 }
 
 let presupuesto; 
@@ -31,11 +37,13 @@ function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cual es tu Presupuesto?');
 
     console.log(Number(presupuestoUsuario)); //me tranforma a numero
-    console.log(isNaN(presupuestoUsuario)); //me dice si es un numero o no en boolean
+    console.log(isNaN(presupuestoUsuario)); //is not a number, es decir verifica si un valor no es un numero
 
     if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload(); //me refresca la pagina
     }
     presupuesto = new Presupuesto(presupuestoUsuario);
     console.log(presupuesto); 
+
+    ui.insertarPresupuesto(presupuesto);
 }
